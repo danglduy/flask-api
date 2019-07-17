4e6c3d3f7e04fc3a8549da941d81999f13755d3b
danglduy <38782010+danglduy@users.noreply.github.com>
2019-07-17 07:59:30 +0700

    Update environment variables

--- config/__init__.py
+++ config/__init__.py
@@ -1,11 +1,11 @@
 import os
 
 class Config(object):
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_PORT = os.getenv('DB_PORT', 3306)
-    DB_NAME = os.getenv('DB_NAME', 'flask')
-    DB_USERNAME = os.getenv('DB_USERNAME', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+    DB_HOST = os.getenv('FLASK_APP_DB_HOST', 'localhost')
+    DB_PORT = os.getenv('FLASK_APP_DB_PORT', 3306)
+    DB_NAME = os.getenv('FLASK_APP_DB_NAME', 'flask')
+    DB_USERNAME = os.getenv('FLASK_APP_DB_USERNAME', 'root')
+    DB_PASSWORD = os.getenv('FLASK_APP_DB_PASSWORD', '')
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
